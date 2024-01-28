4c8197abd6d34d4d1e5e2dc7b6056b4ba15ad814
simba77 <simba7@inbox.ru>
2024-01-28 14:31:55 +0300

    Added deletion of elements when deleting a content type

--- src/Services/ContentTypeService.php
+++ src/Services/ContentTypeService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Johncms\Content\Services;
 
 use Illuminate\Support\Facades\DB;
+use Johncms\Content\Models\ContentElement;
 use Johncms\Content\Models\ContentSection;
 use Johncms\Content\Models\ContentType;
 
@@ -14,6 +15,7 @@ class ContentTypeService
     {
         DB::transaction(function () use ($contentType) {
             ContentSection::query()->where('content_type_id', $contentType->id)->delete();
+            ContentElement::query()->where('content_type_id', $contentType->id)->delete();
             $contentType->delete();
         });
     }
