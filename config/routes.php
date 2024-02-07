4e22d6f683f43e6577e4898c83f17c83779c29a6
simba77 <simba7@inbox.ru>
2024-02-07 22:18:08 +0300

    Updated route definitions

--- config/routes.php
+++ config/routes.php
@@ -11,29 +11,20 @@ return function (RouteCollection $router) {
     $router->group('/admin/content', function (RouteCollection $route) {
         // Types
         $route->get('/', [ContentTypesController::class, 'index'])->setName('content.admin.index');
-        $route->get('/types/create[/]', [ContentTypesController::class, 'create'])->setName('content.admin.type.create');
-        $route->post('/types/create[/]', [ContentTypesController::class, 'create']);
-        $route->get('/types/delete/{id:number}[/]', [ContentTypesController::class, 'delete'])->setName('content.admin.type.delete');
-        $route->post('/types/delete/{id:number}[/]', [ContentTypesController::class, 'delete']);
-        $route->get('/types/edit/{id:number}[/]', [ContentTypesController::class, 'edit'])->setName('content.admin.type.edit');
-        $route->post('/types/edit/{id:number}[/]', [ContentTypesController::class, 'edit']);
+        $route->map(['GET', 'POST'], '/types/create/', [ContentTypesController::class, 'create'])->setName('content.admin.type.create');
+        $route->map(['GET', 'POST'], '/types/delete/{id:number}/', [ContentTypesController::class, 'delete'])->setName('content.admin.type.delete');
+        $route->map(['GET', 'POST'], '/types/edit/{id:number}/', [ContentTypesController::class, 'edit'])->setName('content.admin.type.edit');
 
         // Sections
-        $route->get('/{type:number}[/[{sectionId:number}[/]]]', [ContentSectionsController::class, 'index'])->setName('content.admin.sections');
-        $route->get('/sections/create/{type:number}[/[{sectionId:number}[/]]]', [ContentSectionsController::class, 'create'])->setName('content.admin.sections.create');
-        $route->post('/sections/create/{type:number}[/[{sectionId:number}[/]]]', [ContentSectionsController::class, 'create']);
-        $route->get('/sections/delete/{type:number}/{id:number}[/]', [ContentSectionsController::class, 'delete'])->setName('content.admin.sections.delete');
-        $route->post('/sections/delete/{type:number}/{id:number}[/]', [ContentSectionsController::class, 'delete']);
-        $route->get('/sections/edit/{id:number}[/]', [ContentSectionsController::class, 'edit'])->setName('content.admin.sections.edit');
-        $route->post('/sections/edit/{id:number}[/]', [ContentSectionsController::class, 'edit']);
+        $route->get('/{type:number}/{sectionId:number?}', [ContentSectionsController::class, 'index'])->setName('content.admin.sections');
+        $route->map(['GET', 'POST'], '/sections/create/{type:number}/{sectionId:number?}', [ContentSectionsController::class, 'create'])->setName('content.admin.sections.create');
+        $route->map(['GET', 'POST'], '/sections/delete/{type:number}/{id:number}/', [ContentSectionsController::class, 'delete'])->setName('content.admin.sections.delete');
+        $route->map(['GET', 'POST'], '/sections/edit/{id:number}/', [ContentSectionsController::class, 'edit'])->setName('content.admin.sections.edit');
 
         // Elements
         $route->get('/{type:number}/{sectionId:number}/{elementId:number}[/]', [ContentSectionsController::class, 'index'])->setName('content.admin.elements');
-        $route->get('/elements/create/{type:number}[/[{sectionId:number}[/]]]', [ContentElementsController::class, 'create'])->setName('content.admin.elements.create');
-        $route->post('/elements/create/{type:number}[/[{sectionId:number}[/]]]', [ContentElementsController::class, 'create']);
-        $route->get('/elements/edit/{elementId:number}[/]', [ContentElementsController::class, 'edit'])->setName('content.admin.elements.edit');
-        $route->post('/elements/edit/{elementId:number}[/]', [ContentElementsController::class, 'edit']);
-        $route->get('/elements/delete/{id:number}[/]', [ContentElementsController::class, 'delete'])->setName('content.admin.elements.delete');
-        $route->post('/elements/delete/{id:number}[/]', [ContentElementsController::class, 'delete']);
+        $route->map(['GET', 'POST'], '/elements/create/{type:number}/{sectionId:number?}', [ContentElementsController::class, 'create'])->setName('content.admin.elements.create');
+        $route->map(['GET', 'POST'], '/elements/edit/{elementId:number}/', [ContentElementsController::class, 'edit'])->setName('content.admin.elements.edit');
+        $route->map(['GET', 'POST'], '/elements/delete/{id:number}/', [ContentElementsController::class, 'delete'])->setName('content.admin.elements.delete');
     });
 };
