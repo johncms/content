94503b6384903c19a18b5eb339d992ebee98df7d
simba77 <simba7@inbox.ru>
2024-02-10 12:22:22 +0300

    Added validation of the content section code

--- src/Forms/ContentSectionForm.php
+++ src/Forms/ContentSectionForm.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Johncms\Content\Forms;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
+use Johncms\Content\Models\ContentSection;
 use Johncms\Content\Services\ContentSectionService;
 use Johncms\Forms\AbstractForm;
 use Johncms\Forms\Inputs\InputHidden;
@@ -47,7 +50,24 @@ class ContentSectionForm extends AbstractForm
             ->setLabel(__('Code'))
             ->setPlaceholder(p__('placeholder', 'Enter the Code of the Section'))
             ->setNameAndId('code')
-            ->setValue($this->getValue('code'));
+            ->setValue($this->getValue('code'))
+            ->setValidationRules(
+                [
+                    'ModelNotExists' => [
+                        'model'   => ContentSection::class,
+                        'field'   => 'code',
+                        'exclude' => function (Builder $query) {
+                            $parent = (int) $this->getValue('parent');
+                            if (empty($parent)) {
+                                $parent = null;
+                            }
+                            return $query->where('content_type_id', '=', (int) $this->getValue('content_type_id'))
+                                ->where('parent', '=', $parent)
+                                ->where('id', '!=', $this->getValue('id'));
+                        },
+                    ],
+                ]
+            );
 
         return $fields;
     }
@@ -75,10 +95,13 @@ class ContentSectionForm extends AbstractForm
 
     public function getRequestValues(): array
     {
-        $result = parent::getRequestValues();
-        if (empty($result['parent'])) {
-            $result['parent'] = null;
+        $values = parent::getRequestValues();
+        if (empty($values['parent'])) {
+            $values['parent'] = null;
         }
-        return $result;
+
+        $values['code'] = empty($values['code']) ? Str::slug($values['name']) : Str::slug($values['code']);
+
+        return $values;
     }
 }
